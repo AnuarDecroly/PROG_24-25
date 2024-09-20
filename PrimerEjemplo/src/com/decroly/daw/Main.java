97f6511b8626de0769097fc6bf98bfb0d5990b52
Anuar Khan <akhan@decroly.com>
2024-09-20 16:31:48 +0200

    primer proyecto

---
+++ PrimerEjemplo/src/com/decroly/daw/Main.java
@@ -0,0 +1,11 @@
+package com.decroly.daw;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola Mundo!!!!");
+
+	}
+
+}
