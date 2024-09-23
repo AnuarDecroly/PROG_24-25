37ebd7424c9577d7fbe6827448dac51b5be4914e
Anuar Khan <akhan@decroly.com>
2024-09-23 11:29:03 +0200

    Proyecto ejemplo eclipse

--- PrimerEjemplo/src/com/decroly/daw/Main.java
+++ PrimerEjemplo/src/com/decroly/daw/Main.java
@@ -4,7 +4,37 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola Mundo!!!!");
+		/*
+		 * System.out.print("Hola Mundo!!!!"); System.out.println("Hola Decroly DAW");
+		 */
+		
+		int num = 0;; //vale 0
+		int a = 23;
+		
+		int suma = a + num;
+		
+		double sumaReales = 0.0;
+		
+		double numR = 33.33;
+		
+		boolean isEnable = false;
+		
+		char c = 'S';
+		
+		String text1 = "¿Como te llamas?";
+		String text2 = "Paco";
+		
+		System.out.println(text1+text2);
+		
+		System.out.println("test linea");
+		String respuesta = "Anuar";	
+		
+		System.out.println(text1 + ": " + respuesta);
+		System.out.println("Edad: "+a);
+		
+
+		
+		System.out.println((a != num));
 
 	}
 
