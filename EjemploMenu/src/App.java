38326580c01c521451bdc2e626ea2b761c1427e8
Anuar Khan <akhan@decroly.com>
2024-10-09 08:53:12 +0200

    Ejemplo menu 9-10-2024

---
+++ EjemploMenu/src/App.java
@@ -0,0 +1,85 @@
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+
+        String [] clientes = new String [10];
+        int contador = 0;
+
+        String opcion = "";
+        Scanner reader = new Scanner(System.in);
+
+        do{
+            System.out.println("1 - Insertar cliente");
+            System.out.println("2 - Ver clinetes");
+            System.out.println("3 - Eliminar cliente");
+            System.out.println("4 - Ver facturas");
+            System.out.println("5 - Salir");
+            opcion = reader.nextLine();
+
+            //Ejemplo con if-else
+            if(opcion.equals("1")){
+
+            }
+            else if(opcion.equals("2"))
+            {
+
+            }else if(opcion.equals("3"))
+            {
+
+            }
+            else if(opcion.equals("4")){
+
+            }
+            else if(opcion.equals("5")){
+                //Salir
+            }
+            else{
+
+            }
+
+
+            //Ejemplo con switch - case
+            switch(opcion)
+            {
+                case "1":
+                    reader = new Scanner(System.in);
+                    System.out.println("Introduce el nombre del cliente");
+                    String nombre = reader.nextLine();
+                    clientes[contador] = nombre;
+                    contador ++;
+                    break;
+
+                case "2":
+                    System.out.println("Ver todos los clientes");
+                    for(int i =0; i < clientes.length; i++)
+                    {
+                        if(clientes[i] != null){
+                            System.out.println(clientes[i]);
+                        }
+                    }
+                    break;
+
+                case "3":
+                    System.out.println("Eliminar clientes");
+                    break;
+
+                case "4":
+                    System.out.println("Ver facturas");
+                    break;
+
+                case "5":
+                        System.out.println("Saliendo del programa. Hasta pronto");
+                        break;
+
+                default:
+                        System.out.println("Introduzca una opcion correcta del menu");
+                        break;
+                    
+            }
+
+
+        }while(!opcion.equals("5"));
+    }
+}
--- EjemplosArray/src/App.java
+++ EjemplosArray/src/App.java
@@ -1,43 +1,71 @@
+
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // System.out.println("Hello, World!");
 
-        String [] alumnos = new String [5];
-        int [] notas = new int [alumnos.length];
+        // String [] alumnos = new String [5];
+        // int [] notas = new int [alumnos.length];
 
-        String [] alumnos2 = new String [alumnos.length];
+        // String [] alumnos2 = new String [alumnos.length];
 
-        for(int i = 0; i < alumnos.length; i++){
-            alumnos [i] = "Alumno_"+i;
-        }
-
-        for(int i =0; i< alumnos.length; i++){
-            alumnos2[i] = alumnos[i];
-        }
-
-        // int contador = 0;
-        // while(contador < alumnos.length){
-        //     alumnos[contador] = alumnos[contador] + " Alumno_C"+contador;
-        //     contador++;
+        // for(int i = 0; i < alumnos.length; i++){
+        //     alumnos [i] = "Alumno_"+i;
         // }
 
-        //for each ejemplos
-        for(String al : alumnos){
-            System.out.println(al);
-        }
+        // for(int i =0; i< alumnos.length; i++){
+        //     alumnos2[i] = alumnos[i];
+        // }
 
-        for(int nota : notas){
-            System.out.println(nota);
-        }
+        // // int contador = 0;
+        // // while(contador < alumnos.length){
+        // //     alumnos[contador] = alumnos[contador] + " Alumno_C"+contador;
+        // //     contador++;
+        // // }
 
-        alumnos[0] = "Soy alumno nuevo";
+        // //for each ejemplos
+        // for(String al : alumnos){
+        //     System.out.println(al);
+        // }
 
-        for(int i = 0; i < alumnos.length; i++)
-        {
-            System.out.println(alumnos[i]);
-            System.out.println(alumnos2[i]);
+        // for(int nota : notas){
+        //     System.out.println(nota);
+        // }
 
-        }
+        // alumnos[0] = "Soy alumno nuevo";
+
+        // for(int i = 0; i < alumnos.length; i++)
+        // {
+        //     System.out.println(alumnos[i]);
+        //     System.out.println(alumnos2[i]);
+
+        // }
+
+        int nPositivos = 0, nNegativos = 0;
+        boolean hayNegativos = false;
+        Scanner reader = new Scanner(System.in);
+        int num = 0;
+        do { 
+            System.out.println("Introduce un numero");
+            num = reader.nextInt();
+
+            if(num > 0)
+            {
+                nPositivos ++;
+            }
+            if(num < 0)
+            {
+                hayNegativos = true;
+                nNegativos++;
+            }
+        }while(num != 0);
+
+        System.out.println("Numeros positivos: "+nPositivos);
+        System.out.println("Numeros negativos: "+nNegativos);
+        System.out.println("¿Hay negativos?"+hayNegativos);
+
+
 
     }
 }
