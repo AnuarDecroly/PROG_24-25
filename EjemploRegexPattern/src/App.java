9dfc211c5b840a86c22bbcbc07eca5e7ccdb38f2
Anuar Khan <akhan@decroly.com>
2024-11-22 10:01:53 +0100

    cxz

--- EjemploRegexPattern/src/App.java
+++ EjemploRegexPattern/src/App.java
@@ -3,8 +3,10 @@ public class App {
         //MiUtils.leerDoublePantalla("Introduce tu altura");
         final String patronDNI = "[0-9]{7,8}[A-Za-z]";
         final String pantronIBAN = "[A-Z]{2}[0-9]{22}";
+        final String patronNombre = "[A-Z]{1}[a-z]{2,48}";
 
-        
+        String titular = MiUtils.comprobarPatronRepetidamente(patronNombre, "Introduce tu nombre");
+
         String dni = MiUtils.comprobarPatronRepetidamente(patronDNI, "Introduce tu DNI");
         
         String iban = MiUtils.comprobarPatronRepetidamente(pantronIBAN, "Introduce tu IBAN");
